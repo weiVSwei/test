1cf34f2c647ecb5691c09a8ba18199a0c154c817
崔国伟 <cui@leisu.com>
2018-06-19 15:16:33 +0800

    no message

---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by 崔国伟 on 2018/6/19.
+//  Copyright © 2018年 崔国伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ text/textTests/textTests.m
@@ -0,0 +1,39 @@
+//
+//  textTests.m
+//  textTests
+//
+//  Created by 崔国伟 on 2018/6/19.
+//  Copyright © 2018年 崔国伟. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface textTests : XCTestCase
+
+@end
+
+@implementation textTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
